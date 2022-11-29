dd0796d81e26737ffdd3bdc4c89f6bdb65357b55
AuriseGames <aurisegames@gmail.com>
2022-11-29 20:55:31 +0100

    version final del generador, escenario creado

--- Generador/generador.py
+++ Generador/generador.py
@@ -1,37 +1,51 @@
 # This program prints Hello, world!
 
 import random
+import matplotlib.pyplot as plt
 
+# abrimos el fichero en modo escritura
 f = open("scenary.txt", "w")
 
+# numero de ciudades
 num = 100
 
+# vector de posiciones
 posiciones = []
 
+# iteramos para num ciudades
 for _ in range(num):
     
+    # generamos coordenadas aleatorias
     x = round(random.uniform(0, 100), 2)
     y = round(random.uniform(0, 100), 2)
     
-    repetido = 0
+    # evitamos dos ciudades demasiado juntas
+    repetido = 1
     while repetido == 1:
         repetido = 0
         for t in posiciones:
-            if t == (x,y):
-                print("Repetido")
+            if (abs(t[0] - x) <= 3 and abs(t[1] - y) <= 3):
                 repetido = 1
                 x = round(random.uniform(0, 100), 2)
                 y = round(random.uniform(0, 100), 2)
                 break
-            
+    
+    # añadimos las posiciones al vector
     posiciones.append((x, y))
     
+    # añadimos la linea al fichero
     string = "X: {:05.2f}\tY: {:05.2f}\n".format(x, y)
-    
     f.write(string)
 
+# cerramos el fichero
 f.close()
 
-#open and read the file after the appending:
-# f = open("scenary.txt", "r")
-# print(f.read())
+# generamos el grafico
+for t in posiciones:
+    plt.scatter(t[0], t[1])
+plt.title('Ciudades')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.savefig('scenary.png')
+plt.show()
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AlumnoColaborador
 Repositorio en el que trabajar en el nuevo proyecto de alumno colaborador 2022/2023
+
+INSTRUCCIONES:
+
+- Para crear un nuevo escenario, ejecutar el archivo generador.py
+
+- El resultado se devuelve en forma de dos ficheros scenary.txt y scenary.png en la carpeta en la que estemos situados
+
+- Para que el escenario permanezca y no se sobreescriba, recomendamos guardarlo en una carpeta a parte llamada Escenario
