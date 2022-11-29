d803a87648428c9f8d25e54d0a79ebe643373ce1
AuriseGames <aurisegames@gmail.com>
2022-11-29 20:14:57 +0100

    Generador de escenarios

--- generador.py
+++ generador.py
@@ -5,12 +5,33 @@ import random
 f = open("scenary.txt", "w")
 
 num = 100
+
+posiciones = []
+
 for _ in range(num):
-    string = "X: {}\tY: {}\n".format(round(random.uniform(0, 100), 2), round(random.uniform(0, 100), 2))
+    
+    x = round(random.uniform(0, 100), 2)
+    y = round(random.uniform(0, 100), 2)
+    
+    repetido = 0
+    while repetido == 1:
+        repetido = 0
+        for t in posiciones:
+            if t == (x,y):
+                print("Repetido")
+                repetido = 1
+                x = round(random.uniform(0, 100), 2)
+                y = round(random.uniform(0, 100), 2)
+                break
+            
+    posiciones.append((x, y))
+    
+    string = "X: {:05.2f}\tY: {:05.2f}\n".format(x, y)
+    
     f.write(string)
 
 f.close()
 
 #open and read the file after the appending:
-f = open("scenary.txt", "r")
-print(f.read())
+# f = open("scenary.txt", "r")
+# print(f.read())
