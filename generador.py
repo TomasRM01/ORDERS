dfed6d4becc103665ea130d8916ec70b77690d99
AuriseGames <aurisegames@gmail.com>
2022-11-29 14:32:21 +0100

    Creado generador de escenarios

--- generador.py
+++ generador.py
@@ -1,3 +1,16 @@
 # This program prints Hello, world!
 
-print('Hello, world!')
+import random
+
+f = open("scenary.txt", "w")
+
+num = 100
+for _ in range(num):
+    string = "X: {}\tY: {}\n".format(round(random.uniform(0, 100), 2), round(random.uniform(0, 100), 2))
+    f.write(string)
+
+f.close()
+
+#open and read the file after the appending:
+f = open("scenary.txt", "r")
+print(f.read())
