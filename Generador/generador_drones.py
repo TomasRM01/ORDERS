d558fc72b467362941d6084963ea47204fd85d71
Tomás Ramírez <aurisegames@gmail.com>
2024-02-26 17:51:53 +0100

    Adaptados los scripts generadores y recuperadores de escenarios para el problema de drones y sensores

---
+++ Generador/generador_drones.py
@@ -0,0 +1,72 @@
+import random
+import matplotlib.pyplot as plt
+
+# abrimos el fichero en modo escritura
+f = open("scenary_drones.txt", "w")
+
+# numero de sensores
+num = 100
+
+# coordenadas maximas
+max_x = 100
+max_y = 100
+
+# prioridad maxima
+max_p = 10
+
+# bateria faltante maxima
+max_b = 200
+
+# lista de posiciones, prioridades y baterias
+posiciones = []
+prioridades = []
+baterias = []
+
+# iteramos para num sensores
+for _ in range(num):
+    
+    # generamos coordenadas aleatorias
+    x = round(random.uniform(0, max_x), 2)
+    y = round(random.uniform(0, max_y), 2)
+    
+    # evitamos dos ciudades demasiado juntas
+    repetido = 1
+    while repetido == 1:
+        repetido = 0
+        for t in posiciones:
+            if (abs(t[0] - x) <= 3 and abs(t[1] - y) <= 3):
+                repetido = 1
+                x = round(random.uniform(0, 100), 2)
+                y = round(random.uniform(0, 100), 2)
+                break
+    
+    # generamos una prioridad aleatoria
+    prioridad = random.randint(0, max_p)
+    
+    # generamos una bateria faltante aleatoria
+    bateria = random.randint(0, max_b)
+    
+    # añadimos las posiciones, prioridades y baterias a los vectores
+    posiciones.append((x, y))
+    prioridades.append(prioridad)
+    baterias.append(bateria)
+    
+    # añadimos la linea al fichero
+    string = "X: {:05.2f}\tY: {:05.2f}\tP: {}\tB: {}\n".format(x, y, prioridad, bateria)
+    f.write(string)
+
+# cerramos el fichero
+f.close()
+
+# generamos el grafico
+colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink', 'brown', 'gray', 'cyan']
+for i, t in enumerate(posiciones):
+    color = random.choice(colors)
+    plt.scatter(t[0], t[1], c=color)
+    plt.annotate(f'P: {prioridades[i]}\nB: {baterias[i]}', (t[0], t[1]), textcoords="offset points", xytext=(0,-20), ha='center', fontsize=8)
+plt.title('Sensores')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.savefig('scenary_drones.png', dpi=300)
+plt.show()
+
---
+++ Generador/recuperar_drones.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import re
+import random
+
+# lista de posiciones, prioridades y baterias
+posiciones = []
+prioridades = []
+baterias = []
+
+# abrimos el fichero en modo lectura
+with open("scenary_drones.txt", "r") as f:
+    # pasamos el contenido a un string
+    s = f.read()
+
+# desechamos todo lo que no son numeros y lo convertimos en una lista de elementos
+s = [float(s) for s in re.findall(r'\d+\.?\d*', s)]
+
+# guardamos los elementos en las listas correspondientes
+contador = 0
+for i in range(0, len(s), 4):
+    x = s[i]
+    y = s[i+1]
+    p = s[i+2]
+    b = s[i+3]
+    posiciones.append((x, y))
+    prioridades.append(p)
+    baterias.append(b)
+
+# generamos el grafico
+colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink', 'brown', 'gray', 'cyan']
+for i, t in enumerate(posiciones):
+    color = random.choice(colors)
+    plt.scatter(t[0], t[1], c=color)
+    plt.annotate(f'P: {prioridades[i]}\nB: {baterias[i]}', (t[0], t[1]), textcoords="offset points", xytext=(0,-20), ha='center', fontsize=8)
+plt.title('Sensores')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.savefig('scenary_drones.png', dpi=300)
+plt.show()
